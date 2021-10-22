f37d6975b7a74d7f94ef5c9168b3a7957fe670a1
Hans Knöchel <h.knoechel@lambus.com>
2021-10-22 14:54:52 +0200

    chore: prepare for open source

---
+++ README.md
@@ -0,0 +1,19 @@
+# Titanium iOS Reminders API
+
+Access the native iOS reminders (get, update, delete) in TiDev / Titanium.
+
+## Requirements
+
+- [x] The `NSRemindersUsageDescription` privacy key in your tiapp.xml
+
+## Example
+
+See the `example/app.js` for example!
+
+## Author
+
+Hans Knöchel
+
+## License
+
+MIT
---
+++ dist/modules/iphone/ti.reminders/1.0.0/TiReminders.xcframework/ios-arm64_x86_64-maccatalyst/TiReminders.framework/Versions/A/Headers/TiReminders.h
@@ -0,0 +1,17 @@
+//
+//  TiReminders.h
+//  titanium-reminders
+//
+//  Created by Your Name
+//  Copyright (c) 2021 Your Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TiReminders.
+FOUNDATION_EXPORT double TiRemindersVersionNumber;
+
+//! Project version string for TiReminders.
+FOUNDATION_EXPORT const unsigned char TiRemindersVersionString[];
+
+#import "TiRemindersModuleAssets.h"
